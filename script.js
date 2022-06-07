6b730285601b2788dbb5c5278c96035475bd2810
Gabriel <gabrielalves3654@gmail.com>
2022-06-07 17:00:04 -0300

    ate o requisito 3 com lint passado

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('botao');
 
-function validarEmailSenha () {
-  if(email.value === 'tryber@teste.com' && senha.value === '123456'){
+function validarEmailSenha() {
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
-  }else {
+  } else {
     alert('Email ou senha inválidos.');
-}
+  }
 }
 botao.addEventListener('click', validarEmailSenha);
