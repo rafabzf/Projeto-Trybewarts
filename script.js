c7fbe1fc28075f4ea4977e485adbf842ae88b2e5
Rafael <rafinhabzf@gmail.com>
2022-06-21 09:50:19 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('botao');
+const submiteBtn = document.querySelector('#submit-btn');
+const checkbox = document.querySelector('#agreement');
 
 function validarEmailSenha() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -10,3 +12,12 @@ function validarEmailSenha() {
   }
 }
 botao.addEventListener('click', validarEmailSenha);
+
+function clickDisabled() {
+  if (checkbox.checked !== true) {
+    submiteBtn.disabled = true;
+  } else {
+    submiteBtn.disabled = false;
+  }
+}
+checkbox.addEventListener('click', clickDisabled);
